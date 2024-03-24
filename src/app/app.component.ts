0646e1533a6b7e9b31d3d66978fbfae7abfb1afa
Thomas Hammes <thomas.hammes@verdecard.com.br>
2024-03-24 18:19:21 -0300

    Modals for Add, Editing and Deleting employees are opening and closing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import {Employee} from "./employee/employee";
 import {EmployeeService} from "./employee/employee.service";
 import {HttpErrorResponse} from "@angular/common/http";
 import {CommonModule, NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf],
+  imports: [RouterOutlet, NgForOf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,7 +25,7 @@ export class AppComponent implements OnInit {
 
   public getEmployees(): void {
     this.employeeService.getEmployees().subscribe({
-      next: (response: Employee[]) => {
+      next: (response: Employee[]) => { 
         console.log(response);
         this.employees = response;
       },
@@ -33,6 +34,30 @@ export class AppComponent implements OnInit {
         console.error(error);
       }
     });
+  }
+
+  public onOpenModal(employee: Employee | null, mode: string): void {
+    console.log("OPENED MODAL")
+    console.log(employee);
+    const mainContainer = document.getElementById("main-container")!
+    const button = document.createElement("button");
+    button.type = "button";
+    button.style.display = "none";
+    button.setAttribute("data-toggle", "modal");
+    if (mode === "add") {
+      console.log("add")
+      button.setAttribute("data-target", "#addEmployeeModal");
+    }
+    if (mode === "edit") {
+      console.log("edit")
+      button.setAttribute("data-target", "#updateEmployeeModal");
+    }
+    if (mode === "delete") {
+      console.log("delete")
+      button.setAttribute("data-target", "#deleteEmployeeModal");
+    }
+    mainContainer.appendChild(button);
+    button.click();
   }
 
 }
